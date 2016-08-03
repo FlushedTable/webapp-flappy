99eabe155b844d0d538be0cbee5d84a474dfa689
Nicholas Gawley <anotherplanetuk@gmail.com>
2016-08-03 15:53:55 +0100

    more server changes

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ var server = app.listen((process.env.PORT || 8080), function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Bob's Flappy Bird listening at http://%s:%s", host, port);
+    console.log("Flappy Bird listening at http://%s:%s", host, port);
 });
